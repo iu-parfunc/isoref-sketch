e0eb896a23c5485a6f4845e4a811b29f18434308
Ryan Newton <rrnewton@gmail.com>
2016-07-10 16:36:33 -0400

    Start to sketch out how to lock multiple pieces of ST state with one MVar

---
+++ MVarLock.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE GADTs, ExistentialQuantification #-}
+
+-- | 
+
+module MVarLock where
+
+import Control.Concurrent.MVar
+-- import GHC.Conc
+
+import Control.Monad.ST
+import Data.STRef
+
+--------------------------------------------------------------------------------
+
+data MVarLock s a = MVar a
+
+
+withMVarLock :: MVarLock s a
+             -> (a -> ST s (a, b))
+             -> IO b
+withMVarLock = undefined
+
+
+-- FINISHME:
+-- Need to make make this a higher rank type and then "teleport" the
+-- 'a' result out of it.  Don't let the b result have any mutable bits.
+newMVarLock :: ST s (a,b) -> IO (MVarLock s a, b)
+newMVarLock = undefined
+
+--------------------------------------------------------------------------------
+
+class STCastable (a :: *) where
+  -- What is the 's' param of this type:
+  type GetState a :: *
+  -- What
+  type WithState s a :: *
+
+
+-- Example datatype:
+--------------------------------------------------------------------------------
+
+data MyRec = forall s . MyRec (MVarLock s Bool) (STRef s Int)
+
+mkMyRec :: IO MyRec
+mkMyRec =
+  do (mv,r) <- newMVarLock (do r <- newSTRef 3
+                               return (True, r))
+     return $ MyRec mv r
+
